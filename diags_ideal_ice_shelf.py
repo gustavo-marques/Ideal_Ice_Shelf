e742109de673cfd0f0952fec5bc18599405696f7
Gustavo Marques <gmarques@princeton.edu>
2016-12-21 10:36:19 -0500

    Compute TKE

--- diags_ideal_ice_shelf.py
+++ diags_ideal_ice_shelf.py
@@ -21,7 +21,11 @@ def parseCommandLine():
       ''',
   epilog='Written by Gustavo Marques, Oct. 2016.')
 
+  parser.add_argument('-exp_name', type=str, default='test', help='''Name of the experiment (default is test).''')
+
   parser.add_argument('-energy', type=str, default='', help='''Extract the total energy (KE + PE) per unit mass (m2 s-2) from the given file and plot it as a function of time.''')
+  
+  parser.add_argument('-total_tke', type=str, default='', help='''Plot the total domain turbulent kinetic energy per unit mass (m2 s-2) from the given file.''')
 
   optCmdLineArgs = parser.parse_args()
   driver(optCmdLineArgs)
@@ -34,6 +38,48 @@ def driver(args):
    if args.energy:
       print("Plotting total energy...")
       plot_energy(args)
+
+   if args.total_tke:
+      print("Plotting turbulent kinetic energy...")
+      plot_total_tke(args)
+
+def plot_total_tke(args):
+    file = args.total_tke
+    time = Dataset(file).variables['time'][:]
+    TKEu = np.zeros(len(time))
+    TKEv = np.zeros(len(time))
+    ubar = Dataset(file).variables['u'][0] * 0.; ubar2=0*ubar;
+    vbar = Dataset(file).variables['v'][0] * 0.; vbar2=0*vbar;
+    # using the following identity
+    # <u'^2> = <(u - ubar)^2>, where <> = domain ave; u' = u-ubar
+    #        = <u^2> - <ubar^2>
+    for t in range(len(time)):
+        print 'Time is (days):', time[t]
+        u = Dataset(file).variables['u'][t,:]
+        v = Dataset(file).variables['v'][t,:]
+        ubar = ubar + u
+        ubar2 = ubar2 + u*u
+        vbar = vbar + v
+        vbar2 = vbar2 + v*v
+   
+    ubar, vbar = ubar/len(time), vbar/len(time)
+    print 'Finish computing mean, now tke...'
+    for t in range(len(time)):
+        print 'Time is (days):', time[t]
+        u = Dataset(file).variables['u'][t,:]
+        v = Dataset(file).variables['v'][t,:]
+        up = (u - ubar)**2; vp = (v - vbar)**2
+        TKEu[t] = up.sum() 
+        TKEv[t] = vp.sum() 
+
+     # plot
+    plt.figure()
+    plt.plot(time/365.,TKEu,'r',lw=2.5)
+    plt.plot(time/365.,TKEv,'b',lw=2.5)
+    plt.xlabel('Time [years]')
+    plt.ylabel('Total TKE (red = u, blue = v) [m2 s-2]')
+    plt.grid()
+    plt.savefig(args.exp_name+'_total_tke.png')
 
 def plot_energy(args):
     file = args.energy
