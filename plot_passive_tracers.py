af46f0f1b3fb240d3b6654342f14f13d24fe860e
Gustavo Marques <gmarques@princeton.edu>
2017-04-12 18:37:53 -0400

    Adding option to plot depth-ave vel.

--- plot_passive_tracers.py
+++ plot_passive_tracers.py
@@ -33,6 +33,10 @@ def parseCommandLine():
   parser.add_argument('-ti', type=int, default='0', help='''Initial time indice (default is 0).''')
   parser.add_argument('-tf', type=int, default='-1', help='''Final time indice (default is len(time)).''')
 
+  parser.add_argument('-plot_vel', help='''Plot depth averaged velocity on top of tracer.''', action="store_true")
+
+  parser.add_argument('-nvel', type=int, default='10', help='''Number of grid points to skip when plotting velocity. Default is 10.''')
+
   optCmdLineArgs = parser.parse_args()
   driver(optCmdLineArgs)
 
@@ -85,14 +89,33 @@ def driver(args):
       print 'min/max',data_ave.min(), data_ave.max()
       if args.tracer == 'tr2':
          #dyelev = np.linspace(0,50,100)
-         dyelev = np.linspace(0,15,100)
+         dyelev = np.linspace(0,20,100)
          #dyetic = [0,5,10,25,50]
-         dyetic = [0,2.5,5.0,7.5,10,12.5,15.0]
+         dyetic = [0,5.0,10,15.0,20.0]
       else:
          dyelev = np.linspace(0,1.5,100)
          dyetic = [0,0.2,0.4,0.6,0.8,1.0,1.2,1.4]
 
-      plt_xy(X,Y,data_ave,args.tracer,depth,time,t,dyelev,dyetic,my_cmap,args.name)
+      if args.plot_vel:
+        u = netCDF4.Dataset('prog.nc').variables['u'][tind[t],:,:,:]
+        v = netCDF4.Dataset('prog.nc').variables['v'][tind[t],:,:,:]
+        # u and v at h points
+        uh = np.zeros(u.shape); vh = np.zeros(v.shape);
+        utmp = 0.5 * (u[:,:,0:-1] + u[:,:,1::]) #u_i = 0.5(u_(i+0.5) + u_(i-0.5))
+        vtmp = 0.5 * (v[:,0:-1,:] + v[:,1::,:]) #v_j = 0.5(v_(j+0.5) + v_(j-0.5))
+        uh[:,:,1::] = utmp; uh[:,:,0] = 0.5*u[:,:,0] #u_i=1 = 0.5*u_(i=3/2)
+        vh[:,1::,:] = vtmp; vh[:,0,:] = 0.5*v[:,0,:] #v_j=1 = 0.5*v_(j=3/2)
+        u_ave = (uh * h0).sum(axis = 0)/h0.sum(axis=0)
+        v_ave = (vh * h0).sum(axis = 0)/h0.sum(axis=0)
+        # mask bad values
+        u_ave = np.ma.masked_where(np.abs(u_ave)>10.,u_ave)
+        v_ave = np.ma.masked_where(np.abs(v_ave)>10.,v_ave)
+        print 'u max/min, v max/min', u_ave.max(), u_ave.min(), v_ave.max(), v_ave.min()
+        plt_xy_uv(X,Y,data_ave,u_ave, v_ave, args.tracer,depth,time,t,dyelev,dyetic,my_cmap,args.name,args.nvel)
+      else:
+        plt_xy(X,Y,data_ave,args.tracer,depth,time,t,dyelev,dyetic,my_cmap,args.name)
+
+      print '\n'
 
   print 'Done with loop!'
   return
@@ -120,6 +143,42 @@ def plt_xy(X,Y,tracer,tracer_name,depth,time,t,dyelev,dyetic,my_cmap,exp):
     plt.close()
 
     return
+
+def plt_xy_uv(X,Y,tracer,u,v,tracer_name,depth,time,t,dyelev,dyetic,my_cmap,exp,n):
+    fig = plt.figure(facecolor='black',figsize=(10,10))
+    #fig = plt.figure(facecolor='black')
+    ax = fig.add_subplot(111,axisbg='gray')
+    #ax.axis('equal')
+    cs = ax.contourf(X,Y,tracer,dyelev,cmap=my_cmap,extend='both')
+    ax.set_aspect('equal')
+    cs.set_clim(dyelev.min(),dyelev.max())
+    cb = plt.colorbar(cs,orientation='vertical',ticks=dyetic,extend='both')
+    #cb = plt.colorbar(cs,orientation='horizontal')
+    cb.set_label('Passive tracer', fontsize=16)
+    #ax.plot(xpall,zpall,'k-',lw=0.5)
+    ax.contour(X,Y,depth,[550,650,750,850],colors='k')
+    Q = ax.quiver(X[::n,::n],Y[::n,::n],u[::n,::n],v[::n,::n],scale=5)
+    qk = ax.quiverkey(Q, 0.64, 0.2, 0.25, r'0.25 m s$^{-1}$', labelpos='E',
+                   coordinates='figure')
+    ax.set_xlabel('x [km]')
+    ax.set_ylabel('y [km]')
+    ax.set_xlim(0,500)
+    ax.set_ylim(Y.min(),600)
+    ss = str("ax.set_title('Time: %5.2f days')"% (time))
+    eval(ss)
+    #forceAspect(ax,aspect=1)
+    #plt.grid()
+    ss = str("plt.savefig('PNG/%s-%s-%03d.png',bbox_inches='tight')"% (exp,tracer_name,t))
+    eval(ss)
+    plt.close()
+
+    return
+
+def forceAspect(ax,aspect=1):
+    im = ax.get_images()
+    extent =  im[0].get_extent()
+    ax.set_aspect(abs((extent[1]-extent[0])/(extent[3]-extent[2]))/aspect)
+
 def remap(cs,h0,e0,h,data0,z,depth):
    km,im = h0.shape
    data1 = np.ma.zeros((len(h),im))
