860b9c8b858271f029f30d402eb3d431c66c9c8f
Gustavo Marques <gmarques@princeton.edu>
2017-05-12 15:44:54 -0400

    Update streamfunction calculation

--- misc.py
+++ misc.py
@@ -39,3 +39,35 @@ def make_cmap(colors, position=None, bit=False):
 
     cmap = mpl.colors.LinearSegmentedColormap('my_colormap',cdict,256)
     return cmap
+
+def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
+    '''
+    Function to offset the "center" of a colormap. Useful for
+    data with a negative min and positive max and you want the
+    middle of the colormap's dynamic range to be at zero
+
+    Input
+    -----
+      cmap : The matplotlib colormap to be altered
+      start : Offset from lowest point in the colormap's range.
+          Defaults to 0.0 (no lower ofset). Should be between
+          0.0 and `midpoint`.
+      midpoint : The new center of the colormap. Defaults to
+          0.5 (no shift). Should be between 0.0 and 1.0. In
+          general, this should be  1 - vmax/(vmax + abs(vmin))
+          For example if your data range from -15.0 to +5.0 and
+          you want the center of the colormap at 0.0, `midpoint`
+          should be set to  1 - 5/(5 + 15)) or 0.75
+      stop : Offset from highets point in the colormap's range.
+          Defaults to 1.0 (no upper ofset). Should be between
+          `midpoint` and 1.0.
+    '''
+    cdict = {
+        'red': [],
+        'green': [],
+        'blue': [],
+        'alpha': []
+    }
+
+    # regular index to compute the colors
+    reg_index = np.linspace(start, stop, 257)
--- static_plots/M1/plot_mean_streamfunction.py
+++ static_plots/M1/plot_mean_streamfunction.py
@@ -3,97 +3,13 @@ from netCDF4 import Dataset
 import numpy as np
 #plt.style.use('ggplot')
 import matplotlib
+from matplotlib.colors import Normalize
 matplotlib.rcParams.update({'font.size': 16})
 import sys
 sys.path.append('../../')
 from remapping import mom_remapping
 from misc import *
 import wright_eos as eos
-
-#plt.style.use('ggplot')
-color1 = '#8B4513'
-color2 = '#ff6347'
-color3 = '#8470ff'
-color4 = '#3cb371'
-
-# plot some metrics for runs with varing wind forcing
-
-path='/archive/gmm/Ideal_ice_shelf/Mode1/dx1km/Sigma_zstar/M1_exp5/'
-files = ['out1/prog.nc','out1/prog.nc','out2/prog.nc','out3/prog.nc']
-indices = [12,24,-1,-1]
-titles = ['a)','b)','c)','d)']
-#  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
-x = Dataset(path+'out1/ocean_geometry.nc').variables['geolon'][:]
-y = Dataset(path+'out1/ocean_geometry.nc').variables['geolat'][:]
-depth = Dataset(path+'out1/ocean_geometry.nc').variables['D'][:,250]
-# remapping params
-cs = mom_remapping.Remapping_Cs()
-cs.remapping_scheme = 2
-cs.degree = 2
-z = np.linspace(0,depth.max(),2000)
-h = 0.5* (z[0:-1]+z[1::])
-# grid
-Y,Z = np.meshgrid(y[:,0],h)
-
-def get_data(cs,h,depth,z,exp,t):
-    s=Dataset(exp)
-    time=s.variables['time'][t]/365. # in yrs
-    print 'exp and time (hrs)',exp,time
-    rho=s.variables['rhopot2'][t,:,:,:].mean(axis = 2)
-    v=s.variables['vh'][t,:,:,:].mean(axis = 2)
-    h0=s.variables['h'][t,:,:,:].mean(axis = 2) 
-    e0=s.variables['e'][t,:,:,:].mean(axis = 2) 
-    km, jm = h0.shape
-    # new arrays
-    vh = np.zeros((km,jm)); psi = np.zeros((km,jm))
-    # v at h points
-    vtmp = 0.5 * (v[:,0:-1] + v[:,1::]) #v_j = 0.5(v_(j+0.5) + v_(j-0.5))
-    vh[:,1::] = vtmp; vh[:,0] = 0.5*v[:,0] #v_j=1 = 0.5*v_(j=3/2)
-    # mask and streamfunction
-    
-    vh = np.ma.masked_where(h0<0.001, vh)
-    rho = np.ma.masked_where(h0<0.001, rho)
-    psi[:,:] = (vh.cumsum(axis=0))*0.5
-    #remap
-    psi1=remap(cs,h0,e0,h,psi,z,depth)
-    rho1=remap(cs,h0,e0,h,rho,z,depth)
-
-    return time,rho1,psi1
-
-### Call the function make_cmap which returns your colormap
-#colors = [(255,255,255), (0,0,255), (255,0,0)]
-colors = [(105,105,105),(119,136,153), (0,0,0)]
-my_cmap = make_cmap(colors, bit=True)
-
-i=0
-fig, axes = plt.subplots(nrows=2, ncols=2,sharex='col', sharey='row',figsize=(12,12))
-for ax in axes.flat:
-    time,rho,psi=get_data(cs,h,depth,z,path+files[i],indices[i])
-    cs=ax.contourf(Y,Z,psi,cmap=plt.cm.PuBu)
-    ax.contour(x,y,psi,15,colors='gray')
-    #ax.set_aspect('equal',adjustable = 'box-forced')
-    ax.set_xlim([0,1000])
-    ax.set_ylim([0,2000])
-    ax.set_axis_bgcolor(color1)
-    #cs.set_clim(dyelev.min(),dyelev.max())
-    #psi_vals = np.arange(psi.min(),psi.max(),2)
-    ax.contour(Y,Z,rho,[1037.05,1037.2],colors='k')
-    ss = str("ax.set_title('%s time = %2.1f years')"% (titles[i],time))
-    eval(ss)
-    if i==2:
-       ax.set_xlabel('y [km]', fontsize=18)
-       ax.set_ylabel('depth [m]', fontsize=18)
-
-    i=i+1
-
-fig.subplots_adjust(bottom=0.2)
-cbar_ax = fig.add_axes([0.16, 0.1, 0.7, 0.02])
-cbar=fig.colorbar(cs, orientation='horizontal', cax=cbar_ax, extend='min',ticks=[1.0e-5,1.0e-4,1.0e-3,1.0e-2,1.0e-1,1.0,1.0e1,1.0e2])
-cbar.set_label(r'Depth-averaged melt water tracer', fontsize=16)
-plt.savefig('fig5.png',format='png',dpi=300,bbox_inches='tight')
-plt.close()
-print 'Done!'
-
 
 def remap(cs,h0,e0,h,data0,z,depth):
    """
@@ -131,3 +47,107 @@ def remap(cs,h0,e0,h,data0,z,depth):
         data1[:,i] = np.ma.masked
 
    return data1
+
+#plt.style.use('ggplot')
+color1 = '#8B4513'
+color2 = '#ff6347'
+color3 = '#8470ff'
+color4 = '#3cb371'
+
+# plot some metrics for runs with varing wind forcing
+
+path='/lustre/f1/unswept/Gustavo.Marques/MOM6-examples/ice_ocean_SIS2/IDEAL_IS/dx1km/Sigma_zstar/M1_exp11/'
+files = ['out3/ocean_month.nc','out1/prog.nc','out2/prog.nc','out3/prog.nc']
+indices = [12,24,-12,-12]
+titles = ['a)','b)','c)','d)']
+#  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+x = Dataset(path+'out1/ocean_geometry.nc').variables['geolon'][:]
+y = Dataset(path+'out1/ocean_geometry.nc').variables['geolat'][:]
+depth = Dataset(path+'out1/ocean_geometry.nc').variables['D'][:,250]#.mean(axis=1)
+# remapping params
+cs = mom_remapping.Remapping_Cs()
+cs.remapping_scheme = 2
+cs.degree = 2
+z = np.linspace(0,depth.max(),2000)
+h = 0.5* (z[0:-1]+z[1::])
+# grid
+Y,Z = np.meshgrid(y[:,0],h)
+
+class MidpointNormalize(Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
+
+def get_data(cs,h,depth,z,exp,t):
+    s=Dataset(exp)
+    time=s.variables['time'][t]/365. # in yrs
+    print 'exp and time (hrs)',exp,time
+    temp=s.variables['temp'][:,:,:,250].mean(axis=3).mean(axis=0)
+    salt=s.variables['salt'][:,:,:,250].mean(axis=3).mean(axis=0)
+    rho = eos.wright_eos(temp,salt,2.0e7)
+    v=s.variables['vh'][:,:,:,:].sum(axis=3).mean(axis=0)
+    h0=s.variables['h'][:,:,:,250].mean(axis=0)
+    e0=s.variables['e'][:,:,:,250].mean(axis=0)
+    s.close()
+
+    km, jm = h0.shape
+    # new arrays
+    vh = np.zeros((km,jm)); psi = np.zeros((km,jm))
+    # v at h points
+    vtmp = 0.5 * (v[:,0:-1] + v[:,1::]) #v_j = 0.5(v_(j+0.5) + v_(j-0.5))
+    vh[:,1::] = vtmp; vh[:,0] = 0.5*v[:,0] #v_j=1 = 0.5*v_(j=3/2)
+    # mask and streamfunction
+
+    #vh = np.ma.masked_where(h0<0.001, vh)
+    rho = np.ma.masked_where(h0<0.001, rho)
+    # integrate from bottom to top
+    psi[:,:] = -np.cumsum(vh[::-1,:],axis=0)[::-1,:]*0.5
+    # interpolate psi (can't remap it, it has been integrated already)
+    psi1 = np.zeros((len(h),jm))
+    for j in range(jm):
+       if h0[:,j].sum() > 0.001:
+          tmp = -0.5*(e0[0:-1,j]+e0[1::,j])
+          psi1[:,j] = np.interp(h, tmp, psi[:,j])
+
+       else:
+          psi1[:,j] = -1.0e32
+
+    #remap rho
+    rho1=remap(cs,h0,e0,h,rho,z,depth)
+    # mask bad values
+    psi1 = np.ma.masked_where(np.ma.getmask(rho1), psi1)
+
+    return time,rho1,psi1/1.0e6 # in sv
+
+### Call the function make_cmap which returns your colormap
+colors = [(0,0,255), (255,255,255), (255,0,0)]
+my_cmap = make_cmap(colors, bit=True)
+psi_vals = np.linspace(-0.2,0.025,10)
+norm = MidpointNormalize(midpoint=0)
+time,rho,psi=get_data(cs,h,depth,z,path+files[i],indices[i])
+fig = plt.figure(facecolor='white')
+ax = fig.add_subplot(111,axisbg='black')
+ct=ax.contourf(Y,-Z,psi,psi_vals,cmap=plt.cm.coolwarm, norm=norm)
+plt.colorbar(ct, orientation='horizontal',ticks=[-0.2,-0.15,-0.1,-0.05,0.,0.025])
+#cbar.set_label(r'Depth-averaged melt water tracer', fontsize=16)
+ax.contour(Y,-Z,np.abs(psi),5,colors='gray',linewidth=0.5)
+#ax.set_aspect('equal',adjustable = 'box-forced')
+ax.set_xlim([0,1000])
+ax.set_ylim([-4000,0])
+#ct.set_clim(dyelev.min(),dyelev.max())
+#psi_vals = np.arange(psi.min(),psi.max(),2)
+ax.contour(Y,-Z,rho,[1037.05,1037.2],colors='k')
+ss = str("ax.set_title('%s time = %2.1f years')"% (titles[0],time))
+eval(ss)
+ax.set_xlabel('y [km]', fontsize=18)
+ax.set_ylabel('depth [m]', fontsize=18)
+plt.savefig('fig5.png',format='png',dpi=300,bbox_inches='tight')
+plt.close()
+print 'Done!'
+
